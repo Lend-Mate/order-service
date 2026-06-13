e5cafe65c7422ea13e9fd2358c5cd197c3110532
timurturbil <timurturbil@gmail.com>
2026-06-13 21:03:18 +0300

    fix

--- src/main/java/com/lendmate/orderservice/service/client/ProductServiceClient.java
+++ src/main/java/com/lendmate/orderservice/service/client/ProductServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "product-service")
+@FeignClient(name = "product-service", url = "${product-service.url}")
 public interface ProductServiceClient {
 
     @GetMapping("/products/batch")
