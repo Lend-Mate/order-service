c19b9c73383f27a6d91e3ea16412255446221403
Elif Çelik <elifcelik.eva@gmail.com>
2026-07-28 03:11:47 +0300

    enable OpenTelemetry tracing, and define RestTemplate as a Spring bean to enable trace propagation

--- src/main/java/com/lendmate/orderservice/client/mock/MockClient.java
+++ src/main/java/com/lendmate/orderservice/client/mock/MockClient.java
@@ -1,13 +1,15 @@
 package com.lendmate.orderservice.client.mock;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class MockClient {
-    private final RestTemplate template = new RestTemplate();
+    private final RestTemplate template;
 
     public String getPaymentStatus(Long orderId) {
         log.info("Payment is success");
