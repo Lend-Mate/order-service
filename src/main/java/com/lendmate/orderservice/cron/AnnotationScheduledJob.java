dcbd3c14e1339514c5021eeba77f3ab5b557e01b
timurturbil <timurturbil@gmail.com>
2026-08-21 22:33:09 +0300

    comment scheculed tasks

--- src/main/java/com/lendmate/orderservice/cron/AnnotationScheduledJob.java
+++ src/main/java/com/lendmate/orderservice/cron/AnnotationScheduledJob.java
@@ -1,34 +1,34 @@
-package com.lendmate.orderservice.cron;
-
-import com.lendmate.orderservice.dto.responseDto.OrderResponse;
-import com.lendmate.orderservice.service.OrderService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-import static java.time.LocalTime.now;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class AnnotationScheduledJob {
-    private final OrderService orderService;
-
-    @Scheduled(cron = "#{@everySecondCron}")
-    public void runEverySecondJob() {
-        //log.info("[{}] Job SECONDLY executed - cron loaded from DB via @Scheduled", now());
-        orderService.checkPendingPayments();
-        orderService.convertConfirmedToDelivered();
-    }
-
-    @Scheduled(cron = "#{@everyMinuteCron}")
-    public void runEveryMinuteJob() {
-        //log.info("[{}] Job MINUTELY executed - cron loaded from DB via @Scheduled", now());
-    }
-
-
-
-}
+//package com.lendmate.orderservice.cron;
+//
+//import com.lendmate.orderservice.dto.responseDto.OrderResponse;
+//import com.lendmate.orderservice.service.OrderService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//import static java.time.LocalTime.now;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class AnnotationScheduledJob {
+//    private final OrderService orderService;
+//
+//    @Scheduled(cron = "#{@everySecondCron}")
+//    public void runEverySecondJob() {
+//        //log.info("[{}] Job SECONDLY executed - cron loaded from DB via @Scheduled", now());
+//        orderService.checkPendingPayments();
+//        orderService.convertConfirmedToDelivered();
+//    }
+//
+//    @Scheduled(cron = "#{@everyMinuteCron}")
+//    public void runEveryMinuteJob() {
+//        //log.info("[{}] Job MINUTELY executed - cron loaded from DB via @Scheduled", now());
+//    }
+//
+//
+//
+//}
