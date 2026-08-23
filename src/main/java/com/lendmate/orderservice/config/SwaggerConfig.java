5c48147e7cee52f3d5ec1a0df87d86fd242ee709
timurturbil <timurturbil@gmail.com>
2026-08-23 20:39:29 +0300

    fix swagger

---
+++ src/main/java/com/lendmate/orderservice/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.lendmate.orderservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Order Service API",
+                version = "1.0",
+                description = "JWT Authentication API"
+        ),
+        servers = {
+                @Server(url = "/api/", description = "Default Gateway URL")
+        }
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
